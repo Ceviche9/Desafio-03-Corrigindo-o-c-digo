b9876af4b903ae7ec138e12822f9754800e1acc4
Tundê <ayotunde_sales@hotmail.com>
2022-02-08 12:11:07 -0300

    Desafio-03: Corrigindo o codigo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { request } = require("express");
 const express = require("express");
 
 const { v4: uuid } = require("uuid");
@@ -23,6 +24,8 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
+  repositories.push(repository);
+
   return response.json(repository);
 });
 
@@ -30,7 +33,9 @@ app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  delete updatedRepository.likes
+
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
@@ -48,7 +53,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -66,9 +71,17 @@ app.post("/repositories/:id/like", (request, response) => {
     return response.status(404).json({ error: "Repository not found" });
   }
 
+  const repository = repositories.find(repository => repository.id === id);
+
+  if(!repository) {
+    return response.status(404).json({ error: "Repository not found" });
+  }
+
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  repository.likes = likes
+
+  return response.json({likes});
 });
 
 module.exports = app;
